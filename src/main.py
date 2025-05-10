484bc7f45bf7782fe738f1e36d49561f238b74bf
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2025-05-09 19:54:49 -0700

    Updated href and src basepaths

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import sys
 from generator import static_to_public, generate_pages_recursive
 # print(test)
 
-basepath = sys.argv[0] if sys.argv else "/"
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/your-repo-name"
 
 static_to_public("static", "docs")
 # generate_pages_recursive("content", "template.html", "public", basepath)
